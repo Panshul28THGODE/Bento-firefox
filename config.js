2adc30be9f81c9e6e94524bd60d245f83e64ad5d
Panshul28THGODE <95143864+Panshul28THGODE@users.noreply.github.com>
2024-02-23 17:37:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'd6aa4078391f19db2dbce13b5d84804d', // Write here your API Key
+	weatherKey: '0586bf94e62a0be52ab47ea666abeccd', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
